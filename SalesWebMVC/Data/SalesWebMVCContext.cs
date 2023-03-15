80278cb87b711fd6700773c43102d428b37e0bb0
Carlos Pereira <Jocause@hotmail.com>
2023-03-15 05:41:46 +0000

    Seeding Service

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Data
 {
-    class SalesWebMVCContext : DbContext
+    public class SalesWebMVCContext : DbContext
     {
         public SalesWebMVCContext (DbContextOptions<SalesWebMVCContext> options)
             : base(options)
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
-        public DbSet<SalesWebMVC.Models.Seller> Seller { get; set; }
-        public DbSet<SalesWebMVC.Models.SalesRecord> SalesRecord { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
