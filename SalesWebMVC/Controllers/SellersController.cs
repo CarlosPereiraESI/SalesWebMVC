d46576976882218089f8b04805bb8d48f225aae8
CarlosPereiraESI <Jocause@hotmail.com>
2023-03-29 07:17:35 +0100

    Returning custom error page

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,110 +4,117 @@ using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using System.Collections.Generic;
 using SalesWebMVC.Services.Exceptions;
+using System.Diagnostics;
+using System;
 
 namespace SalesWebMVC.Controllers
 {
 	public class SellersController : Controller
-    {
-        private readonly SellerService _sellerService;
-        private readonly DepartmentService _departmentService;
-        public SellersController(SellerService sellerService, DepartmentService departmentService)
-        {
-            _sellerService = sellerService;
-            _departmentService = departmentService;
-        }
-        public IActionResult Index()
-        {
-            var list = _sellerService.FindAll();
-            return View(list);
-        }
+	{
+		private readonly SellerService _sellerService;
+		private readonly DepartmentService _departmentService;
+		public SellersController(SellerService sellerService, DepartmentService departmentService)
+		{
+			_sellerService = sellerService;
+			_departmentService = departmentService;
+		}
+		public IActionResult Index()
+		{
+			var list = _sellerService.FindAll();
+			return View(list);
+		}
 
-        public IActionResult Create()
-        {
-            var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments };
-            return View(viewModel);
-        }
+		public IActionResult Create()
+		{
+			var departments = _departmentService.FindAll();
+			var viewModel = new SellerFormViewModel { Departments = departments };
+			return View(viewModel);
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
-        {
-            _sellerService.Insert(seller);
-            return RedirectToAction(nameof(Index));
-        }
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var seller = _sellerService.FindSellerById(id.Value);
-            if (seller == null)
-            {
-                return NotFound();
-            }
-            return View(seller);
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
-        {
-            _sellerService.DeleteSeller(id);
-            return RedirectToAction(nameof(Index));
-        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Seller seller)
+		{
+			_sellerService.Insert(seller);
+			return RedirectToAction(nameof(Index));
+		}
+		public IActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+			}
+			var seller = _sellerService.FindSellerById(id.Value);
+			if (seller == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
+			}
+			return View(seller);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(int id)
+		{
+			_sellerService.DeleteSeller(id);
+			return RedirectToAction(nameof(Index));
+		}
 
-        public IActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var seller = _sellerService.FindSellerById(id.Value);
-            if (seller == null)
-            {
-                return NotFound();
-            }
-            return View(seller);
-        }
+		public IActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+			}
+			var seller = _sellerService.FindSellerById(id.Value);
+			if (seller == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
+			}
+			return View(seller);
+		}
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var obj = _sellerService.FindSellerById(id.Value);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            List<Department> departments = _departmentService.FindAll();
-            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
-            return View(viewModel);
-        }
+		public IActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not provided" });
+			}
+			var obj = _sellerService.FindSellerById(id.Value);
+			if (obj == null)
+			{
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
+			}
+			List<Department> departments = _departmentService.FindAll();
+			SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+			return View(viewModel);
+		}
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Seller seller)
-        {
-            if (id != seller.Id)
-            {
-                return BadRequest();
-            }
-            try
-            {
-                _sellerService.Update(seller);
-                return RedirectToAction(nameof(Index));
-            }
-            catch (NotFoundException)
-            {
-                return NotFound();
-            }
-            catch (DbConcurrencyException)
-            {
-                return BadRequest();
-            }
-        }
-    }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(int id, Seller seller)
+		{
+			if (id != seller.Id)
+			{
+				return BadRequest();
+			}
+			try
+			{
+				_sellerService.Update(seller);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (ApplicationException e)
+			{
+				return RedirectToAction(nameof(Error), new { message = e.Message });
+			}
+		}
+		public IActionResult Error(string message)
+		{
+			var viewModel = new ErrorViewModel
+			{
+				Message = message,
+				RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+			};
+			return View(viewModel);
+		}
+	}
 }
