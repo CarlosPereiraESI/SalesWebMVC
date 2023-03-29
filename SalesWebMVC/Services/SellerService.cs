d2ef0defcb3cf54cd817a5239b40bede41970bcb
CarlosPereiraESI <Jocause@hotmail.com>
2023-03-29 03:27:19 +0100

    Delete Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -5,24 +5,36 @@ using System.Linq;
 
 namespace SalesWebMVC.Services
 {
-    public class SellerService
-    {
-        private readonly SalesWebMVCContext _context;
+	public class SellerService
+	{
+		private readonly SalesWebMVCContext _context;
 
-        public SellerService(SalesWebMVCContext context)
-        {
-            _context = context;
-        }
+		public SellerService(SalesWebMVCContext context)
+		{
+			_context = context;
+		}
 
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.ToList();
-        }
+		public List<Seller> FindAll()
+		{
+			return _context.Seller.ToList();
+		}
 
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
-    }
+		public void Insert(Seller obj)
+		{
+			_context.Add(obj);
+			_context.SaveChanges();
+		}
+
+		public Seller FindSellerById(int id)
+		{
+			return _context.Seller.FirstOrDefault(x => x.Id == id);
+		}
+
+		public void DeleteSeller(int id)
+		{
+			var seller = _context.Seller.Find(id);
+			_context.Remove(seller);
+			_context.SaveChanges();
+		}
+	}
 }
